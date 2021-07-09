3580b349d3e03f9f42955e9f5acc1b891f0c2fd3
Smriti Rani <smritixrani29@gmail.com>
2021-07-09 13:01:59 +0530

    todo

--- todo-app-v1/src/Components/todoAdd.js
+++ todo-app-v1/src/Components/todoAdd.js
@@ -4,18 +4,18 @@ function todoAdd() {
     return (
         <div className = "todo-add">
             <form className = "todo-add-form">
-                <label>Add a new Todo:</label>
+                <label>Add a Task:</label>
                 <input type = "text" placeholder = "Enter here" className = "todo-add-new-item" />
-                <button className = "close-todo-add-page">
-                    <i className = "close-icon">
-                        <img src = "src/Icons/close.png"></img>
-                    </i>
-                </button>
-                <button className = "create-todo-button">Add</button>
-                <button className = "cancel-todo-button">Cancel</button>
-                    
-                
+                <label for="Priority-menu">Priority</label>
+                    <select id="priority-menu-list">
+                    <option value="High">High</option>
+                    <option value="Mid">Mid</option>
+                    <option value="Low">Low</option>
+                    </select>
+               
             </form>
+            <button className = "create-todo-button">Add</button>
+            <button className = "cancel-todo-button">Cancel</button>
             
         </div>
     )
--- todo-app-v1/src/Components/todoform.js
+++ todo-app-v1/src/Components/todoform.js
@@ -2,28 +2,37 @@ import React from 'react'
 
 function todoform() {
 
-
     return (
+        <div className = "todo-form-container">
             <form className = 'todo-form'>
-                <input
-                type = 'text'
-                placeholder='Search'
-                name='text'
-                className='todo-search'
-                
-                />
+                <input type = 'text' placeholder='Search' name='text'className='todo-search'></input>
                 <i class = "todo-search-icon">
                     <img src = "src/Icons/magnifying-glass.png"></img>
                 </i>
-
-            <button className='todo-add-button'>
-                <i class = "add-icon">
-                    <img src = "src/Icons/add.png"></img>
+                <button className='todo-add-button'>
+                    <i className = "add-icon">
+                        <img src = "src/Icons/add.png"></img>
+                    </i>
+                </button>
+            </form>
+            <form className = "todo-filter-form">
+            <button className = "todo-filter-button">
+                <i className = "filter-icon">
+                    <img src = "src/Icons/filter-filled-tools-symbol.png"></img>
+                    <label for="Filter-menu"></label>
+                    <select id="Filter-menu-list">
+                    <option value="Date">Date</option>
+                    <option value="Time">Time</option>
+                    <option value="PriorityA">Priority-Ascending</option>
+                    <option value="PriorityD">Priority-Descending</option>
+                    </select>
                 </i>
             </button>
-
-
             </form>
+
+            
+        </div>
+            
        
             
             
